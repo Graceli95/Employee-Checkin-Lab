85d03302d155e8852b4ab48331c5116cf1a609e2
Graceli95 <graceli9095@gmail.com>
2024-12-03 21:15:45 -0700

    add files

--- src/HourlyEmp.java
+++ src/HourlyEmp.java
@@ -5,7 +5,7 @@ public class HourlyEmp extends Employee {
 // Invoke the parent class constructor to initialize parameters
     public HourlyEmp(String firstName, String lastName, String title, double payRate, double hours) {
         super(firstName, lastName, title, payRate);
-        this.hoursWorked = hoursWorked;
+        this.hoursWorked = hours;
     }
 
     public double calculateHourlySalary() {
